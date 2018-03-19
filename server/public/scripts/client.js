7398c52868c7d0942d6cb4e383fb3db256d47f2f
Blake Burbach <cras.bburbach@gmail.com>
2018-03-19 09:57:20 -0500

    interface and client side started.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,20 @@ $(document).ready(readyNow);
 
 function readyNow(){
     console.log('Oh! Hey there!');
-}
+    $('#clearButton').on('click', function(){
+        
+    }); // end clear button on click
+    $('#doMathButton').on('click', function(){
+        console.log('in doMathButton on click');
+        // capture user input & store in an object
+        let objectToSend = {
+            x: $('#xIn').val(),
+            y: $('#yIn').val(), 
+            type: $('#typeIn').val()
+        };
+        console.log('objectToSend', objectToSend);
+        // send object to server
+        // display answe on DOM
+    }); // end doMathButton on click
+} // end doc ready
+
